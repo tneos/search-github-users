6d003c70512203d4c70f8140e348cc4ee41a5f30
Thomas Neos <tneos@hotmail.com>
2025-07-30 11:19:07 +0100

    Add title Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import Title from "./components/ui/title/Title";
 import SearchForm from "./components/ui/form/SearchForm";
 import UserProfile from "./components/ui/user/UserProfile";
 
@@ -6,7 +7,8 @@ const App = () => {
   const [userName, setUserName] = useState("steve-wilson");
 
   return (
-    <main className="mx-auto max-w-6xl px-8 py-20">
+    <main className="mx-auto max-w-6xl px-8 py-8">
+      <Title />
       <SearchForm userName={userName} setUserName={setUserName} />
       <UserProfile userName={userName} />
     </main>
---
+++ src/components/ui/title/Title.tsx
@@ -0,0 +1,8 @@
+const Title = () => {
+  return (
+    <div className="w-full mb-16">
+      <h1 className="montserrat-title w-full text-4xl text-center">Search Github Users</h1>
+    </div>
+  );
+};
+export default Title;
