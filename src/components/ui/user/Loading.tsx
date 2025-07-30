7fee2b9a16a6dae04bc2d5e095613626b5673c5f
Thomas Neos <tneos@hotmail.com>
2025-07-30 10:01:15 +0100

    Adjust Loading skeleton to different screen sizes

--- src/components/ui/user/Loading.tsx
+++ src/components/ui/user/Loading.tsx
@@ -2,9 +2,11 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const Loading = () => {
+  const screenSize = window.innerWidth;
+
   return (
     <div className="w-full">
-      <Skeleton className="h-[194px] mb-8 rounded" width={500} />
+      <Skeleton className="h-[194px] mb-8 rounded" width={screenSize > 800 ? "50%" : "100%"} />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-2 mb-8">
         <Skeleton className="h-[70px] rounded" />
         <Skeleton className="h-[70px] rounded" />
